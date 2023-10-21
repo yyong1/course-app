ac2bce899a26ac5407b811642830f3f6ede989d0
Andrey Baranov <baranov.a.64103@gmail.com>
2023-10-22 00:37:24 +0200

    user controller, repo, service template

--- src/main/java/com/example/coursesystem/rest/controllers/UserController.java
+++ src/main/java/com/example/coursesystem/rest/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.example.coursesystem.rest.controllers;
 
 import com.example.coursesystem.core.model.User;
 import com.example.coursesystem.core.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
