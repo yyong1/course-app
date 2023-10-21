37cd034dff7a16bfa7a8a956be1ccbca0bd612a1
Andrey Baranov <baranov.a.64103@gmail.com>
2023-10-21 21:25:01 +0200

    user model

---
+++ src/main/java/com/example/coursesystem/core/model/User.java
@@ -0,0 +1,52 @@
+package com.example.coursesystem.core.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String username;
+
+    private String password;
+
+
+    private String email;
+
+
+    private String role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
