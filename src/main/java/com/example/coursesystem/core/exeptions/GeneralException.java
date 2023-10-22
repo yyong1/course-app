a4c6d498ea24edca90f8cd1eaf1dde2f0e529ec4
Andrey Baranov <baranov.a.64103@gmail.com>
2023-10-22 15:18:57 +0200

    general exceptions

---
+++ src/main/java/com/example/coursesystem/core/exeptions/GeneralException.java
@@ -0,0 +1,26 @@
+package com.example.coursesystem.core.exeptions;
+
+public class GeneralException extends RuntimeException {
+    private int httpCode = 500;
+
+    public GeneralException(int httpCode) {
+        this.httpCode = httpCode;
+    }
+
+    public GeneralException(int httpCode, String message) {
+        super(message);
+        this.httpCode = httpCode;
+    }
+
+    public GeneralException(String message, Exception e) {
+        super(message, e);
+    }
+
+    public GeneralException(Exception e) {
+        super(e);
+    }
+
+    public GeneralException(String message) {
+        super(message);
+    }
+}
