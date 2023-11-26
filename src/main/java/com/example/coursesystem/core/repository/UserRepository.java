874f2a4643198230c3bdf0c4d685a488bd731189
Andrey Baranov <baranov.a.64103@gmail.com>
2023-11-26 22:14:46 +0100

    find by email method

--- src/main/java/com/example/coursesystem/core/repository/UserRepository.java
+++ src/main/java/com/example/coursesystem/core/repository/UserRepository.java
@@ -23,4 +23,6 @@ public interface UserRepository extends MongoRepository<User, String> {
 
     @Query(value="{$or:[{email:'?0'}, {username:'?0'}]}")
     Optional<User> findByUsernameOrEmail(String username);
+
+    User findByEmail(String email);
 }
