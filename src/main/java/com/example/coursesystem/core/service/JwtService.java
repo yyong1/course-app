0d6d7748667304b967963b0ac48b1fdf1d486e96
Andrey Baranov <baranov.a.64103@gmail.com>
2024-01-27 19:59:35 +0100

    Logger in case jwt changed

--- src/main/java/com/example/coursesystem/core/service/JwtService.java
+++ src/main/java/com/example/coursesystem/core/service/JwtService.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -25,38 +27,40 @@ public class JwtService {
     @Value("${security.jwt.refresh-token.expire-length}")
     private long refreshTokenValidityInMilliseconds;
 
-    // +++
+    private static final Logger logger = LoggerFactory.getLogger(JwtService.class);
+
+
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
         final Claims claims = extractAllClaims(token);
         return claimsResolvers.apply(claims);
     }
 
-    // +++
     private Claims extractAllClaims(String token) {
-        return Jwts
+        try {
+            return Jwts
                 .parser()
                 .verifyWith(getSigningKey())
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
+            } catch (Exception e) {
+                logger.error("Error extracting claims: {}", e.getMessage());
+                throw e;
+            }
     }
 
-    // method is used to get the user from a parsed token.
     public String extractUserName(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    // +++
     private SecretKey getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(jwtSigningSecret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    // +++
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return generateToken(extraClaims, userDetails, validityInMilliseconds);
     }
-    // +++
     private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails, long validityInMilliseconds) {
         return Jwts.builder()
                 .claims(extraClaims)
@@ -66,17 +70,14 @@ public class JwtService {
                 .signWith(getSigningKey())
                 .compact();
     }
-    // +++
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String userName = extractUserName(token);
         return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
-    // +++
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
-    // +++
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
@@ -84,6 +85,4 @@ public class JwtService {
     public String generateRefreshToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails, refreshTokenValidityInMilliseconds);
     }
-
-
 }
